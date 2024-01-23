0ef1ec3a420465d3faef392e057095f5bd2c9781
Aneliq Peteva <peteva_a@abv.bg>
2024-01-23 09:40:25 +0200

    add

--- ADVANCED PYTHON/ADVANCED PYTHON/4. Multidimensional Lists/EXERCISE 1/07. Snake Moves.py
+++ ADVANCED PYTHON/ADVANCED PYTHON/4. Multidimensional Lists/EXERCISE 1/07. Snake Moves.py
@@ -1,66 +1,33 @@
-def find_player_position():
-    for row in range(rows):
-        if "P" in matrix[row]:
-            return row, matrix[row].index("P")
+# from collections import deque
+#
+# rows, cols = [int(x) for x in input().split()]
+# word = list(input())
+#
+# word_copy = deque(word)
+#
+# for row in range(rows):
+#     while len(word_copy) < cols:
+#         word_copy.extend(word)
+#
+#     if row % 2 == 0:
+#         print(*[word_copy.popleft() for _ in range(cols)], sep="")
+#     else:
+#         print(*[word_copy.popleft() for _ in range(cols)][::-1], sep="")
+#
 
-def bunnies_position():
-    position = []
+# solution 2
 
-    for row in range(rows):
-        for col in range(cols):
-            if matrix[row][col] == "B":
-                position.append([row, col])
-    return position
-
-def bunnies_move(bunnies_position):
-    for row, col in bunnies_position:
-        for bunnie_move in directions.values():
-            new_row, new_col = row + bunnie_move[0], col + bunnie_move[1]
-
-            if check_valid_index(new_row, new_col):
-                matrix[new_row][new_col] = "B"
-
-def check_valid_index(row, col, player = False):
-    global wins
-
-    if 0 <= row < rows and 0 <= col < cols:
-        return True
-    if player:
-        wins = True
-def show_results(status="won"):
-    [print(*row, sep="") for row in matrix]
-    print(f"{status}: {player_row} {player_col}")
-
-    exit()
-
-def check_player_alive(row,col):
-    if matrix[row][col] == "B":
-        show_results("dead")
+from collections import deque
 
 rows, cols = [int(x) for x in input().split()]
-matrix = [list(input()) for _ in range(rows)]
+word = list(input())
 
-commands = input()
-wins = False
-directions = {
-    "U": (-1, 0),
-    "D": (1, 0),
-    "R" : (0, 1),
-    "L" : (0, -1)
-}
+word_copy = deque(word)
+multiplier = 1
 
-player_row, player_col = find_player_position()
-matrix[player_row][player_col] = "."
+for row in range(rows):
+    while len(word_copy) < cols:
+        word_copy.extend(word)
 
-for command in commands:
-    player_movement_row, player_movement_col = player_row + directions[command][0], player_col + directions[command][1]
-
-    if check_valid_index(player_movement_row, player_movement_col, True):
-        player_row, player_col = player_movement_row, player_movement_col
-
-    bunnies_move(bunnies_position())
-
-    if wins:
-        show_results()
-
-    check_player_alive(player_row, player_col)
+    print(*[word_copy.popleft() for _ in range(cols)][::multiplier], sep="")
+    multiplier *= -1
