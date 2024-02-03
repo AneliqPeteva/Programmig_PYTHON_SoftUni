d7322cd60e40a32957a3c301d3015315b4bc535b
Aneliq Peteva <peteva_a@abv.bg>
2024-02-03 23:52:12 +0200

    add

--- ADVANCED PYTHON/ADVANCED PYTHON/EXAM/PA Retake Exam - 13 December 2023/03. SoftUni Students.py
+++ ADVANCED PYTHON/ADVANCED PYTHON/EXAM/PA Retake Exam - 13 December 2023/03. SoftUni Students.py
@@ -1,35 +1,27 @@
 def softuni_students(*args, **kwargs):
-    id_name = {}
-    id_courses = {}
-    result = ""
     invalid_students = []
     valid_students = {}
-    for elements in args:
-        name = elements[1]
-        id = elements[0]
+    for id_for_args, name in args:
+        if id_for_args in kwargs.keys():
+            valid_students[name] = kwargs[id_for_args]
+        else:
+            invalid_students.append(name)
 
-        if not id in id_name:
-            id_name[id] = []
-        id_name[id].append(name)
+    result = ""
+    if valid_students:
+        for name_student, name_courses in sorted(valid_students.items()):
+            result += f"*** A student with the username {name_student} has successfully finished the course {name_courses}!\n"
+    if invalid_students:
+        result += f"!!! Invalid course students: {', '.join(sorted(invalid_students))}"
 
-    for id, name_courses in kwargs.items():
-        if not id in id_courses:
-            id_courses[id] = []
-        id_courses[id].append(name_courses)
-
-
-
-
-    for id_for_name, name_people in id_name.items():
-        if id_for_name in id_courses:
-            if not
+    return result
 
 
 
-print(softuni_students(
-    ('id_7', 'Silvester1'),
-    ('id_32', 'Katq21'),
-    ('id_7', 'The programmer'),
-    id_76='Spring Fundamentals',
-    id_7='Spring Advanced',
-))
+# print(softuni_students(
+#     ('id_7', 'Silvester1'),
+#     ('id_32', 'Katq21'),
+#     ('id_7', 'The programmer'),
+#     id_76='Spring Fundamentals',
+#     id_7='Spring Advanced',
+# ))
