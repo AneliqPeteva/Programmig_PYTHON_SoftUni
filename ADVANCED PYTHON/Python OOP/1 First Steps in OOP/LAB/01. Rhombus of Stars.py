15ed80dbf4251de392dabded8078d13894c471b7
Aneliq Peteva <peteva_a@abv.bg>
2024-02-24 23:59:55 +0200

    add

--- ADVANCED PYTHON/Python OOP/1 First Steps in OOP/LAB/01. Rhombus of Stars.py
+++ ADVANCED PYTHON/Python OOP/1 First Steps in OOP/LAB/01. Rhombus of Stars.py
@@ -1,28 +1,41 @@
-def print_row(n, count):
-    print(' ' * (n - count), end="")
-    print(*['*'] * count)
+# def print_row(n, count):
+#     print(' ' * (n - count), end="")
+#     print(*['*'] * count)
+#
+#
+# def print_triangle(n):
+#     for count in range(1, n + 1):
+#         print_row(n, count)
+#
+#
+# def print_reversed_triangle(n):
+#     for count in range(n - 1, 0, -1):
+#         print_row(n, count)
+#
+#
+# def print_rhombus(n):
+#     print_triangle(n)
+#     print_reversed_triangle(n)
+#
+#
+# def print_square(n):
+#     for _ in range(n):
+#         print_row(n, n)
+#
+#
+# n = int(input())
+#
+# print_rhombus(n)
 
+def print_upper_rhombus(n):
+    for row in range(1, n+1):
+        print(" " * (n-row), "* "*row)
 
-def print_triangle(n):
-    for count in range(1, n + 1):
-        print_row(n, count)
-
-
-def print_reversed_triangle(n):
-    for count in range(n - 1, 0, -1):
-        print_row(n, count)
-
-
-def print_rhombus(n):
-    print_triangle(n)
-    print_reversed_triangle(n)
-
-
-def print_square(n):
-    for _ in range(n):
-        print_row(n, n)
-
+def print_botton_rhombus(n):
+    for row in range(n - 1, 0, -1):
+        print(" " * (n - row), "* " * row)
 
 n = int(input())
 
-print_rhombus(n)
+print_upper_rhombus(n)
+print_botton_rhombus(n)
