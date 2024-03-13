c502123e2420f5221b14829a2d7d11d09ec54a07
Aneliq Peteva <peteva_a@abv.bg>
2024-03-13 09:58:24 +0200

    add

---
+++ ADVANCED PYTHON/Python OOP/3 Inheritance  /LAB/06 Stack of Strings/stack.py
@@ -0,0 +1,38 @@
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, el):
+        self.data.append(el)
+
+    def pop(self):
+        return self.data.pop()
+
+    def top(self):
+        return self.data[-1]
+
+    def is_empty(self):
+        return False if self.data else True
+
+    def __str__(self):
+        reversed_data = reversed(self.data)
+        result = ", ".join(reversed_data)
+        return f"[{result}]"
+
+    # test zero
+    import unittest
+
+    class StackTests(unittest.TestCase):
+        def test_zero(self):
+            stack = Stack()
+            stack.push("apple")
+            stack.push("carrot")
+            self.assertEqual(str(stack), '[carrot, apple]')
+            self.assertEqual(stack.pop(), 'carrot')
+            self.assertEqual(stack.top(), 'apple')
+            stack.push("cucumber")
+            self.assertEqual(str(stack), '[cucumber, apple]')
+            self.assertEqual(stack.is_empty(), False)
+
+    if __name__ == '__main__':
+        unittest.main()
