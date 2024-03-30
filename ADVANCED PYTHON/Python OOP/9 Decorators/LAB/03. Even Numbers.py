cd214e50cbb66d7c78a0bfec891ad9ca1b03d7a7
Aneliq Peteva <peteva_a@abv.bg>
2024-03-30 21:49:59 +0200

    add

---
+++ ADVANCED PYTHON/Python OOP/9 Decorators/LAB/03. Even Numbers.py
@@ -0,0 +1,17 @@
+
+def even_numbers(func):
+    def wrapper(numbers):
+        result = func(numbers)
+        return [n for n in result if n % 2 == 0]
+
+    return wrapper
+
+
+# Test code:
+#
+# @even_numbers
+# def get_numbers(numbers):
+#     return numbers
+#
+#
+# print(get_numbers([1, 2, 3, 4, 5]))
